40e6f9a47cb958785b03e1407ce846770b306671
emblero <emblerooo@mail.ru>
2025-05-01 17:14:20 +0300

    fddf

---
+++ discovery-service1/src/test/java/org/example/discoveryservice1/DiscoveryService1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.discoveryservice1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryService1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
